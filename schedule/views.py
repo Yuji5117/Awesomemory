8bc6aab3006b20b56e5620cf02212792dbb5cf5d
Yuji <ny5117@icloud.com>
2020-01-18 11:13:30 +0900

    added user-specific-func in schedule and post app

--- schedule/views.py
+++ schedule/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Schedule
 from .forms import ScheduleCreateForm
 
+
 class ScheduleListView(LoginRequiredMixin, ListView):
     model = Schedule
     template_name = 'schedule/schedule_list.html'
@@ -13,21 +14,32 @@ class ScheduleListView(LoginRequiredMixin, ListView):
     #upcoming schedule
     queryset = Schedule.objects.order_by('date')
 
+    def get_queryset(self):
+        return Schedule.objects.filter(created_by=self.request.user)
+
+
 class ScheduleDetailView(LoginRequiredMixin, DetailView):
     model = Schedule
     template_name = 'schedule/_detail.html'
+
 
 class ScheduleCreateView(LoginRequiredMixin, CreateView):
     model = Schedule
     form_class =  ScheduleCreateForm
     template_name = 'schedule/schedule_create.html'
     success_url = reverse_lazy('schedule:schedule')
+    
+    def form_valid(self, form):
+        form.instance.created_by_id = self.request.user.id
+        return super().form_valid(form)
+
 
 class ScheduleUpdateView(LoginRequiredMixin, UpdateView):
     model = Schedule
     form_class =  ScheduleCreateForm
     template_name = 'schedule/schedule_create.html'
     success_url = reverse_lazy('schedule:schedule')
+
 
 class ScheduleDeleteView(LoginRequiredMixin, DeleteView):
     model = Schedule
