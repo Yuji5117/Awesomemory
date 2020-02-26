c83bbe515e70163efd47d61e92cab1469596cdf9
Yuji Nakamura <50049575+Yuji5117@users.noreply.github.com>
2020-02-26 14:05:55 +0900

    styled css, html and js

---
+++ static/js/header.js
@@ -0,0 +1,13 @@
+// JQUERY //
+$(function() {
+
+    // FOR HEADER NAV //
+    $("#menu-toggle").on('click', function() {
+        $('.menu-bar ul').toggleClass('menu-lists-active');
+    });
+
+
+    // $("#menu-toggle").on('click', function() {
+    //     $('.menu-bar ul').toggleClass('menu-lists-active');
+    // });
+});
